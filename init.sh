837faa7f8ac132b4c2cca98d4c710ae308af2e16
Sanghun <po4865@gmail.com>
2024-11-18 20:12:57 +0100

    chore: Delete shell script files

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DORKER_INSTALL_DIR=$(dirname ${BASH_SOURCE:-$0})
-source $DORKER_INSTALL_DIR/src/settings.sh &&
-source $DORKER_INSTALL_DIR/src/docker.sh &&
-source $DORKER_INSTALL_DIR/src/dorker.sh &&
-[[ $DORKER_ECHO_ON_STARTUP -eq 1 ]] &&
-echo $DORKER_GREEN"Dorker commands successfully loaded"$DORKER_WHITE
